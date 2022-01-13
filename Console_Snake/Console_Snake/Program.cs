c1f5936d7b6fb3ed8cc63e2612c2dca1de7b9883
OvcS <gerkules007@yahoo.com>
2022-01-13 21:22:14 +0300

    List<Point> add

--- Console_Snake/Console_Snake/Program.cs
+++ Console_Snake/Console_Snake/Program.cs
@@ -11,8 +11,30 @@ namespace Console_Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(4,5,'*');
+            Point p2 = new Point(7, 1, '#');
+            Point p3 = new Point(2, 4, '%');
+            Point p4 = new Point(6, 2, '(');
             p1.Draw();
-            
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList) Console.WriteLine(i);
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
             Console.ReadKey();
         }
     }
