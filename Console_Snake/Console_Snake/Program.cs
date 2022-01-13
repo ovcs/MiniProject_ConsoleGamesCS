e727c97024bdbbb6820ccc4a79e95620d2d88177
OvcS_WorkPC <ovc.semyon@gmail.com>
2022-01-13 16:24:49 +0300

    Add Draw()

--- Console_Snake/Console_Snake/Program.cs
+++ Console_Snake/Console_Snake/Program.cs
@@ -10,8 +10,16 @@ namespace Console_Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x1 = 4, x2 = 5;
+            char sym1 = '*';
+            Draw(x1,y1,sym1);
             Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
