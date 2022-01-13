6c2eb8464354c1eab26036a8d60d9b1a67660b0e
OvcS <gerkules007@yahoo.com>
2022-01-13 21:41:25 +0300

    Create HorisontalLine class

---
+++ Console_Snake/Console_Snake/HorisontalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Snake
+{
+    class HorisontalLine
+    {
+        List<Point> pList;
+
+        public HorisontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+            
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in pList) p.Draw();
+        }
+    }
+}
--- Console_Snake/Console_Snake/Program.cs
+++ Console_Snake/Console_Snake/Program.cs
@@ -16,24 +16,8 @@ namespace Console_Snake
             Point p4 = new Point(6, 2, '(');
             p1.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList) Console.WriteLine(i);
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
+            HorisontalLine line = new HorisontalLine(1,30,8,'+');
+            line.Drow();
 
             Console.ReadKey();
         }
