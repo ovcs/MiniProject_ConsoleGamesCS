4e28032fc9897cc5af1725802b1faf3f0648fd93
OvcS <gerkules007@yahoo.com>
2022-01-14 22:10:12 +0300

    Add FoodCreator class

---
+++ Console_Snake/Console_Snake/FoodCreator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Snake
+{
+    class FoodCreator
+    {
+        int mapStart;
+        int mapWidth;
+        int mapHeight;
+        char sym;
+
+        Random rnd = new Random();
+        
+        public FoodCreator(int mapStart, int mapWidth, int mapHeight, char sym)
+        {
+            this.mapStart = mapStart;
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            this.sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = new Random().Next(mapStart + 2, mapWidth - 2);
+            int y = new Random().Next(mapStart + 2, mapHeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
