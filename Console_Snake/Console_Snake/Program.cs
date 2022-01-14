1717c86a56c1b10cf141cab4d8ec6fc407f54154
OvcS <gerkules007@yahoo.com>
2022-01-14 22:45:19 +0300

    Add Food on Action Screen

--- Console_Snake/Console_Snake/Program.cs
+++ Console_Snake/Console_Snake/Program.cs
@@ -26,15 +26,28 @@ namespace Console_Snake
             Point p = new Point(dWidth / 2, dHeight / 2, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            
+
+            FoodCreator foodCreator = new FoodCreator(dStart, dWidth, dHeight, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while(true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
                 if (Console.KeyAvailable)
                 {
                     snake.MoveKeys(Console.ReadKey());
                 }
-                snake.Move();
-                Thread.Sleep(500);
+                Thread.Sleep(300);
             }
         }
     }
