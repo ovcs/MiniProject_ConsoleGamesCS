6cb87e4235fab4b04da1e35302b32a379627f9c0
OvcS <gerkules007@yahoo.com>
2022-01-24 14:48:36 +0300

    Add CreateWindow() in Programm.cs

--- Console_Tetris/Console_Tetris/Console_Tetris/Program.cs
+++ Console_Tetris/Console_Tetris/Console_Tetris/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Console_Tetris
@@ -10,18 +11,26 @@ namespace Console_Tetris
     {
         static void Main(string[] args)
         {
-            int dStart = 0, dWidth = 39, dHeight = 39;
+
+            int dStart = 0, dWidth = 50, dHeight = 35;
             char filler1 = '+';
             char filler2 = 'o';
 
-            Console.SetWindowSize(dWidth + 1, dHeight + 1);
-            Console.SetBufferSize(dWidth + 2, dHeight + 1);
+            CreateWindow(dWidth, dHeight);
             
             Point p = new Point(5, 10, filler1);
             p.Output();
             Console.ReadKey();
             p.Clear();
             Console.ReadKey();
+        }
+        
+        static void CreateWindow(int width, int height)
+        {
+            int setWidth = Console.LargestWindowWidth >= width ? width : Console.LargestWindowWidth;
+            int setHeight = Console.LargestWindowHeight >= height ? height : Console.LargestWindowHeight;
+            Console.SetWindowSize(setWidth, setHeight);
+            Console.SetBufferSize(setWidth+1, setHeight);
         }
     }
 }
